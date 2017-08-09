1b7e6b222ef55f9dbb2b9742c90232e1f917549b
user9 <zassyed@ifi.uio.no>
2017-08-09 22:36:50 +0000

    Added a test next in GildedRoseTest.java

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -14,4 +14,12 @@ public class GildedRoseTest {
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    public void next() {
+        Item[] items = new Item[] { new Item("next", 0, 0) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("next", app.items[0].name);
+    }
+
 }
